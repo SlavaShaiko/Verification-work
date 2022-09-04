680d3c017aedbbf8dcf64759a912a01bb55bd3b4
SlavaShaiko <v.shaiko2011@yandex.ru>
2022-09-04 17:03:21 +0300

    added working solution for Task

--- Task/Program.cs
+++ Task/Program.cs
@@ -7,10 +7,12 @@
 //["1234","1567","-2","computer science"] :-> ["-2"]
 //["Russia","Denmark","Kazan"] :-> []
 
-
 Console.Clear();
-Console.Write("Введите количество строк: ");
+Console.Write("Введите количество строк в массиве: ");
 int numberOfRows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите длину строки: ");
+int stringLength = Convert.ToInt32(Console.ReadLine());
 
 string[] fillingTheArray = FillingTheArray(numberOfRows);
 string[] FillingTheArray(int numberOfRows)
@@ -18,10 +20,9 @@ string[] FillingTheArray(int numberOfRows)
     string[] array = new string[numberOfRows];
     for (int i = 0; i < array.Length; i++)
     {
-        Console.Write($"Введите {i + 1} строку: ");
+        Console.Write($"Введите {i + 1} строку:");
         string text = Convert.ToString(Console.ReadLine());
-        if (i < array.Length - 1) array[i] = ($"{text}, ");
-        else array[i] = ($"{text}");
+        array[i] = text;
     }
     return array;
 }
@@ -33,52 +34,28 @@ void PrintArray(string[] array)
     for (int i = 0; i < array.Length; i++)
     {
         if (i == 0) Console.Write("[");
-        if (i < array.Length - 1) Console.Write(array[i]);
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
         else Console.Write(array[i] + "]");
     }
 }
 
-string[] sortedArray = SortedArray(fillingTheArray);
-string[] SortedArray(string[] metod)
+string[] sortedArray = SortedArray(fillingTheArray, stringLength);
+string[] SortedArray(string[] metod, int length)
 {
     string[] array = new string[metod.Length];
-    int count = 0;
     for (int i = 0; i < metod.Length; i++)
     {
-        if (metod[i].Length <= 5)
+        if (metod[i].Length <= length)
         {
-            array[count] = metod[i];
-            count++;
+            array[i] = metod[i];
         }
     }
     return array;
 }
+
 Console.WriteLine("");
 Console.Write("Отсортированный массив с длиной строк меньше либо равно 3 символам: ");
 PrintArray(sortedArray);
+Console.WriteLine("");
 
-
-// string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
-// string[] array2 = new string[array1.Length];
-// void SecondArrayWithIF(string[] array1, string[] array2)
-// {
-//     int count = 0;
-//     for (int i = 0; i < array1.Length; i++)
-//     {
-//     if(array1[i].Length <= 3)
-//         {
-//         array2[count] = array1[i];
-//         count++;
-//         }
-//     }
-// }
-// void PrintArray(string[] array)
-// {
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         Console.Write($"{array[i]} ");
-//     }
-//     Console.WriteLine();
-// }
-// SecondArrayWithIF(array1, array2);
-// PrintArray(array2);
+//Что-то я завис не могу понять, как не выводить пустое значение из string[] sortedArray!
